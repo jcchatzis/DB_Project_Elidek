4d78a106908c5b517fc9aa7baaa89d2fb8394f5d
John Chatzis <106564144+jcchatzis@users.noreply.github.com>
2022-06-02 14:33:06 +0300

    Add files via upload

---
+++ queries/QUERY_3.6.sql
@@ -0,0 +1,14 @@
+/* QUERY 3.6 */
+
+select count(b.researcher_id),
+       a.researcher_last_name,
+       a.researcher_first_name
+from researcher a,
+     res_works_on_project b,
+     project c
+where a.researcher_id = b.researcher_id and
+      b.project_id = c.project_id and
+      a.researcher_birth_date > now() - interval 40 year and
+      c.end_date < now()
+group by a.researcher_last_name, a.researcher_first_name
+order by count(b.researcher_id) desc
