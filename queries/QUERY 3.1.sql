19a7c6e5fd7149ed43aaee7d84bead3d5ee62684
John Chatzis <106564144+jcchatzis@users.noreply.github.com>
2022-06-04 21:38:55 +0300

    complete 3.1 query

--- queries/QUERY 3.1.sql
+++ queries/QUERY 3.1.sql
@@ -18,3 +18,18 @@ where a.start_date = (?) and
       c.employer_name = (?)
   
 order by a.project_id asc
+
+
+/*------*/
+select a.researcher_last_name,
+       a.researcher_first_name,
+       a.researcher_id
+
+from researcher a join res_works_on_project b on (a.researcher_id=b.researcher_id)
+                  join project c on (b.project_id=c.project_id)
+  
+where c.project_title = (?)
+
+group by a.researcher_last_name, a.researcher_first_name, a.researcher_id
+
+order by a.researcher_id asc
