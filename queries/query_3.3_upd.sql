5fa0294ac5407f2654e220def6ae60444dd7f2ea
SabosThanos <62442286+SabosThanos@users.noreply.github.com>
2022-06-04 20:12:26 +0300

    Update query_3.3_upd.sql

--- queries/query_3.3_upd.sql
+++ queries/query_3.3_upd.sql
@@ -2,8 +2,9 @@
 
 select a.project_title
 from project a join scientific_field_project b on (a.project_id = b.project_id)
+               join scientific_field c on (b.sc_field_id = c.sc_field_id)
 where a.date_end >= now() and
-      b.sc_field_id= (?)
+      c.name_of_sc_f=(?)
 order by a.project_title asc
 
 
@@ -15,9 +16,10 @@ from researcher a join researcher_works_org b on ( a.researcher_id = b.researche
                   join project c on (b.org_id = c.org_id)
                   join res_works_on_project d on (c.project_id = d.project_id and a.researcher_id = d.researcher_id)
                   join scientific_field_project e on (c.project_id = e.project_id)
+                  join scientific_field f on (e.sc_field_id = f.sc_field_id )  
 where c.date_end >= now() and
       (b.date_end_of_work is null or year(b.date_end_of_work) >= year(now())) and
-      e.sc_field_id= (?)
+      f.name_of_sc_f= (?)
 order by a.researcher_last_name asc,
          a.researcher_first_name asc
          
