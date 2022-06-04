b7dc328f20d83327a586eba0fcb429fbfac2f874
John Chatzis <106564144+jcchatzis@users.noreply.github.com>
2022-06-04 20:43:25 +0300

    typos corrections

--- queries/QUERY 3.2.sql
+++ queries/QUERY 3.2.sql
@@ -3,19 +3,18 @@ select c.project_title
 from researcher a join res_works_on_project b on (a.researcher_id = b.researcher_id )
                   join project c on (b.project_id = c.project_id)
 
-where a.researcher_last_name = (??) and
-      a.researcher_first_name =(??)
+where a.researcher_last_name = (?)  and
+      a.researcher_first_name = (?)
   
 order by c.project_id asc
 
 
-
 /*------*/
 
-select  c.project_title
+select  b.project_title
            
-from organisation a join project b on (a.project_id = b.project_id)
+from organisation a join project b on (a.org_id = b.org_id)
 
-where a.org_name = (??)
+where a.org_name = (?)
   
-order by c.project_id) desc
+order by b.project_id desc
