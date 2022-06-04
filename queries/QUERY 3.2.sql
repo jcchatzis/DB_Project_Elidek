8b362e362710918959d3bc476d84bc7ffa90cf50
SabosThanos <62442286+SabosThanos@users.noreply.github.com>
2022-06-04 20:26:22 +0300

    Update QUERY 3.2.sql

--- queries/QUERY 3.2.sql
+++ queries/QUERY 3.2.sql
@@ -1,23 +1,21 @@
-select count (c.project_id),
-    c.project_title;
+select c.project_title;
            
-from researcher a join res_works_on_project b on (a.researcher_id = b.resarcher)
+from researcher a join res_works_on_project b on (a.researcher_id = b.researcher_id )
                   join project c on (b.project_id = c.project_id)
 
-where a.researcher_id = (??) 
+where a.researcher_last_name = (??) and
+      a.researcher_first_name =(??)
   
-order by count (c.project_id) desc
+order by c.project_id asc
 
 
 
 /*------*/
 
-select count (c.project_id),
-    c.project_title;
+select  c.project_title;
            
-from scientific_field a join scientific_field_project b on (a.sc_field_id =b.sc_field_id)
-                        join project c on (b.project_id = c.project_id)
+from organisation a join project b on (a.project_id = b.project_id)
 
-where a.sc_field_id = (??)
+where a.org_name = (??)
   
-order by count(c.project_id) desc
+order by c.project_id) desc
